34fa5fc50190b57f70a35ba7355835eb5a9841ad
Seiju-N <seiju.nomura@gmail.com>
2023-09-01 18:50:40 +0900

    bug fix

--- event_manager.py
+++ event_manager.py
@@ -28,6 +28,18 @@ def has_role(member: discord.Member, role_name: str) -> bool:
     return any(role.name == role_name for role in member.roles)
 
 
+def create_event_embed(event_name, participants, spectators):
+    participants_str = " ".join(participants) if participants else "なし"
+    spectators_str = " ".join(spectators) if spectators else "なし"
+    embed = discord.Embed(
+        title=event_name,
+        description=f"🤚 参加者: {participants_str}\n"
+        f"👀 観戦者: {spectators_str}\n"
+        "リアクションで参加・観戦を選択してください。",
+    )
+    return embed
+
+
 class EventCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -41,9 +53,7 @@ class EventCog(commands.Cog):
         ):
             await ctx.send("You don't have permission to create an event!")
             return
-        embed = discord.Embed(
-            title=event_name, description="React to join or spectate!"
-        )
+        embed = create_event_embed(event_name, 0, 0)
         message = await ctx.send(embed=embed)
         self.bot.event_message_ids.append(message.id)
 
@@ -98,18 +108,38 @@ class ReactionRoleCog(commands.Cog):
         if role is None:
             return
         member = guild.get_member(payload.user_id)
-        if role == self.emoji_to_role[discord.PartialEmoji(name="❌")]:
-            await member.remove_roles(role)
-        else:
-            try:
-                await member.add_roles(role)
-            except discord.HTTPException:
-                pass
+        if role.name == ROLE_NAME_JOIN and has_role(member, ROLE_NAME_SPEC):
+            spec_role = guild.get_role(get_role_id_by_name(guild, ROLE_NAME_SPEC))
+            await member.remove_roles(spec_role)
+        elif role.name == ROLE_NAME_SPEC and has_role(member, ROLE_NAME_JOIN):
+            join_role = guild.get_role(get_role_id_by_name(guild, ROLE_NAME_JOIN))
+            await member.remove_roles(join_role)
+
+        await member.add_roles(role)
+
         channel = self.bot.get_channel(payload.channel_id)
         if isinstance(channel, discord.TextChannel):
             message = await channel.fetch_message(payload.message_id)
             user = self.bot.get_user(payload.user_id)
+
+            # 参加者と観戦者の名前のリストを取得
+            participants = await self.get_users_with_reaction(message, "✋")
+            spectators = await self.get_users_with_reaction(message, "👀")
+            new_embed = create_event_embed(
+                message.embeds[0].title, participants, spectators
+            )
+
+            # メッセージのEmbedを更新
+            await message.edit(embed=new_embed)
             await message.remove_reaction(payload.emoji, user)
+
+    async def get_users_with_reaction(self, message, emoji_name):
+        """メッセージから指定された絵文字を持つユーザーの名前のリストを取得"""
+        for reaction in message.reactions:
+            if str(reaction.emoji) == emoji_name:
+                users = await reaction.users().flatten()
+                return [f"<@{user.id}>" for user in users if not user.bot]
+        return []
 
 
 bot.add_cog(EventCog(bot))
